68cbd33efb5ba4972607232a151e49de6dc13e8e
thausifullah <thausifullah@gmail.com>
2021-12-06 11:27:52 +0530

    first commit

---
+++ src/notes.js
@@ -0,0 +1,23 @@
+const notes =[
+{
+    key: 1,
+    title: " Day-1 : ooty",
+    content: "  Doddabetta Peak ",
+},
+{
+    key: 2,
+    title: "Day-2 : ooty",
+    content: " Government Rose Garden ",
+},
+{
+    key: 3,
+    title: "Day-3 : munnar",
+    content: "Mattupetty Dam  ",
+},
+{
+    key: 4,
+    title: "Day-4 : munnar",
+    content: "Rajamalai National Park ",
+}
+]
+export default notes;
